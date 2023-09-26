cf65b4b9d76d62c291f6be3d735eb275c4c25a9d
brezhy <127342001+brezhy@users.noreply.github.com>
2023-09-26 18:16:20 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        double primer;
+        double segon;
+        primer = 7;
+        segon = 3;
+        double suma = primer + segon;
+        double resta = primer - segon;
+        double multiplicacio = primer * segon;
+        double divisio = primer / segon;
+
+        //Suma
+        System.out.println(suma);
+        System.out.println(resta);
+        System.out.println(multiplicacio);
+        System.out.println(divisio);
+    }
+}
