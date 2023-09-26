a0a766e44d2be91cd55e40fa81a4b5029f018d63
brezhy <127342001+brezhy@users.noreply.github.com>
2023-09-26 18:32:27 +0200

    Exercici 1 acabat

--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,23 @@
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        double primer;
-        double segon;
-        primer = 7;
-        segon = 3;
+        // Declaració i inicialització de les variables
+        double primer = 7;
+        double segon = 3;
+
+// Càlculs
         double suma = primer + segon;
         double resta = primer - segon;
         double multiplicacio = primer * segon;
         double divisio = primer / segon;
 
-        //Suma
-        System.out.println(suma);
-        System.out.println(resta);
-        System.out.println(multiplicacio);
-        System.out.println(divisio);
+// Exercici 1: Mostra els resultats dels càlculs
+        System.out.println("La suma de " + primer + " i " + segon + " és igual a " + suma);
+        System.out.println("La resta de " + primer + " i " + segon + " és igual a " + resta);
+        System.out.println("La multiplicació de " + primer + " i " + segon + " és igual a " + multiplicacio);
+        System.out.println("La divisió de " + primer + " i " + segon + " és igual a " + divisio);
+
+
+// Exercici 2: Mitjana nota alumnes
     }
 }
